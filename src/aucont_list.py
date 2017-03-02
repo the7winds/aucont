49db05d5c83fd2bdf11c8314752cbf860efedba3
Gleb Valin <the7winds@yandex.ru>
2017-03-02 20:27:49 +0300

    fixed aucont_list

--- src/aucont_list.py
+++ src/aucont_list.py
@@ -5,8 +5,8 @@ import os
 BASE = os.environ['AUCONT']
 
 def main(): 
-	containers = filter(os.path.isdir, os.listdir(BASE))
-	processes = filter(os.path.isdir, os.listdir('/proc'))
+	containers = filter(lambda d: os.path.isdir(os.path.join(BASE, d)), os.listdir(BASE))
+	processes = filter(lambda d: os.path.isdir(os.path.join('/proc', d)), os.listdir('/proc'))
 	alive = set(containers) & set(processes)
 
 	for pid in alive:
